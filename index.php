9504b2aec01abb57dfed93a90639c6b8faebc72d
izzulm <muhizzul16@gmail.com>
2020-04-23 23:40:24 +0700

    kedua commit nih

--- index.php
+++ index.php
@@ -1,5 +1,59 @@
-<?php
+<?php 
 
-echo "Hello Wolrd! Its PHP";
+// echo("Izzul");
+// $nama = "Izzul";
+// Interpolasi dengan menggunakan kutip dua jgn kutip satu
+// echo "Halo Nama Saya $nama";
+
+// //operator
+// $a = 9;
+// $b = 10;
+// echo $a + $b;
+
+//penggabung string dengan . <-
+// $nama_depan = "Izzul";
+// $nama_belakang = "islam";
+
+// echo  $nama_depan . " " . $nama_belakang;
+
+//assignment
+//= , += , -=, /= , %= , .=
+// $x = "Izzul";
+// $x .= "Islam";
+
+// echo $x;
+
+
+//perbandingan
+// , > <= >= ==
+// $a = "abc";
+// $b = "abcd";
+//var_dump(5 == "5");
+
+//identitas
+//mengecek type data nilai
+// // === , !===
+// var_dump(5 === "5");
+
+//logika
+// && , || , !
+$x = 10 ;
+
+var_dump($x < 5 || $x % 2 == 0);
+
 
 ?>
+
+<!-- <!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Belajar PHP
+	</title>
+</head>
+<body>
+	<h1>Halo, Ini bersama <?php echo $nama ?></h1>
+
+	
+</body>
+</html> -->
